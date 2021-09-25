9b61b03d8be3fbcc62512bad361fa5cf29781dfd
NewLk <newtonafr@hotmail.com>
2021-09-25 18:24:38 -0300

    console object
    some examples of usage for the console object.

--- JS_SANDBOX/app.js
+++ JS_SANDBOX/app.js
@@ -1,1 +1,24 @@
 alert("This is alert");
+
+console.log('Hello World');
+
+var humans = "love";
+console.log(humans);
+
+console.log([1,2,3,4]);
+
+console.log({a: 1, b: 2});
+
+console.table({a: 1, b: 2});
+
+console.clear();
+
+console.error("Something happened");
+
+console.warn("Watch out");
+
+console.time("whatever");
+  console.log("whatever");
+  console.log("whatever");
+  console.log("whatever");
+console.timeEnd("whatever");
